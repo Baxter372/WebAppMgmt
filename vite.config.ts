d424c144470bd819bc60de57c4a7e0d66d7bb98f
Baxter372 <32847620+Baxter372@users.noreply.github.com>
2025-11-30 15:41:36 -0600

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/WebAppMgmt/',
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Suppress all warnings
+        return;
+      }
+    }
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  }
 })
